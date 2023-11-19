bb3601f47f3e176a5c0757d1f33df7fa29a780dc
gustjtmd <shseo@datanuri.net>
2023-11-19 15:17:21 +0900

    4. 단어 뒤집기

---
+++ src/String/Chap04.java
@@ -0,0 +1,45 @@
+package String;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 4. 단어 뒤집기
+ *
+ * 설명
+ *  - N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요.
+ *
+ *  입력
+ *   첫 줄에 자연수N(3<=N<=20)이 주어집니다.
+ *   두번째 줄 부터 N개의 단어가 각 줄에 하나씩 주어집니다 단어는 알파벳으로만 구성되어 있습니다.
+ *
+ *   출력
+ *    N개의 단어를 입력된 순서대로 한 줄에 하나씩 뒤집어서 출력합니다.
+ */
+
+public class Chap04 {
+    public ArrayList<String> solution(int n, String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+
+        for(String x : str){
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Chap04 T = new Chap04();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for(int i=0; i<n; i++){
+            str[i] = kb.next();
+        }
+
+        for(String x : T.solution(n, str)){
+            System.out.println(x);
+        }
+    }
+}
